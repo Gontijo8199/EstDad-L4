fc371a51bc97fb4b57d372e03d7c8542dc1f33af
Gontijo8199 <rafael.gontijof2006@gmail.com>
2026-06-04 12:30:15 -0300

    build: Reestruturação inicial do projeto

---
+++ src/Trie.cpp
@@ -0,0 +1,16 @@
+#include<Trie.hpp>
+
+
+TrieNode::TrieNode(){}
+TrieNode::~TrieNode(){}
+
+Trie::Trie(){}
+Trie::~Trie(){}
+
+bool Trie::insert(Game* game){}
+bool Trie::contains(std::string title){}
+
+std::vector<Game*> Trie::autocomplete(std::string prefix, int k){}
+
+std::string Trie::toSearchKey(std::string text){}
+void Trie::sortResults(std::vector<Game*>& games){}
